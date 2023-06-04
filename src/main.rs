8424ae28f82b1b622cd132fd5b1d8087960a7b1e
Vijith Kusumba <vijith.messi@gmail.com>
2023-06-04 12:56:24 +0530

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,8 @@ mod store;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
     let main_store = Arc::new(Mutex::new(Store::new()));
-
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
     loop {
